28857d7ba485ce4f7f7d0b8ec0fc5b4160f82b85
alicia-colom <aliciacolom@gmail.com>
2020-10-21 22:38:23 +0200

    Fix problems

--- src/js/image-components.js
+++ src/js/image-components.js
@@ -16,8 +16,8 @@ const profilePreview = document.querySelector('.js__profile-preview');
  */
 function getImage(e) {
   const myFile = e.currentTarget.files[0];
-  fr.addEventListener('load', writeImage);
-  fr.readAsDataURL(myFile);
+	fr.addEventListener('load', writeImage);
+	fr.readAsDataURL(myFile);
 }
 
 /**
--- src/js/main.js
+++ src/js/main.js
@@ -1,47 +1,47 @@
 /* eslint-disable indent */
-"use strict";
+'use strict';
 
 // Colapsables
 // constantes de header (cada uno funciona por separado), arrow (no usado aun) y content (cada uno funciona por separado)
-const header1 = document.querySelector(".js-header1");
-const header2 = document.querySelector(".js-header2");
-const header3 = document.querySelector(".js-header3");
+const header1 = document.querySelector('.js-header1');
+const header2 = document.querySelector('.js-header2');
+const header3 = document.querySelector('.js-header3');
 // const arrow = document.querySelector('.js-arrow');
-const content1 = document.querySelector(".js-content1");
-const content2 = document.querySelector(".js-content2");
-const content3 = document.querySelector(".js-content3");
+const content1 = document.querySelector('.js-content1');
+const content2 = document.querySelector('.js-content2');
+const content3 = document.querySelector('.js-content3');
 
 // funcion para el colapsable de diseña
 function collapse1() {
-	content1.classList.toggle("collapsed");
+	content1.classList.toggle('collapsed');
 	// si diseña NO contiene la clase .collapsed, se le añade a los otros dos apartados
-	if (!content1.classList.contains("collapsed")) {
-		content2.classList.add("collapsed");
-		content3.classList.add("collapsed");
+	if (!content1.classList.contains('collapsed')) {
+		content2.classList.add('collapsed');
+		content3.classList.add('collapsed');
 	}
 }
 // añadimos llamador de evento a el header de diseña
-header1.addEventListener("click", collapse1);
+header1.addEventListener('click', collapse1);
 
 // repetimos lo mismo, para rellena
 function collapse2() {
-	content2.classList.toggle("collapsed");
-	if (!content2.classList.contains("collapsed")) {
-		content1.classList.add("collapsed");
-		content3.classList.add("collapsed");
+	content2.classList.toggle('collapsed');
+	if (!content2.classList.contains('collapsed')) {
+		content1.classList.add('collapsed');
+		content3.classList.add('collapsed');
 	}
 }
-header2.addEventListener("click", collapse2);
+header2.addEventListener('click', collapse2);
 
 // y para comparte
 function collapse3() {
-	content3.classList.toggle("collapsed");
-	if (!content3.classList.contains("collapsed")) {
-		content1.classList.add("collapsed");
-		content2.classList.add("collapsed");
+	content3.classList.toggle('collapsed');
+	if (!content3.classList.contains('collapsed')) {
+		content1.classList.add('collapsed');
+		content2.classList.add('collapsed');
 	}
 }
-header3.addEventListener("click", collapse3);
+header3.addEventListener('click', collapse3);
 
 // END Colapsable
 
@@ -50,29 +50,30 @@ header3.addEventListener("click", collapse3);
 // END Selector de color
 
 // Text input
-const inputList = document.querySelectorAll(".js-field");
+const inputList = document.querySelectorAll('.js-field');
 
 let data = {
-	name: "Nombre Apellido",
-	job: "Front-end developer",
-	phone: "#",
-	email: "#",
-	linkedin: "#",
-	github: "#",
-	photo: "",
+	name: 'Nombre Apellido',
+	job: 'Front-end developer',
+	phone: '#',
+	email: '#',
+	linkedin: '#',
+	github: '#',
+	photo: 'url(../images/NAN-card-photo-default.jpg)',
 	palette: 1,
 };
 
-const previewText = document.querySelectorAll(".js-preview-text");
+const previewText = document.querySelectorAll('.js-preview-text');
 console.log(previewText);
 const nameInit = previewText[0].innerHTML;
-console.log("Esto es nameInit " + nameInit);
+console.log('Esto es nameInit ' + nameInit);
 const jobInit = previewText[1].innerHTML;
-console.log("Esto es jobInit " + jobInit);
+console.log('Esto es jobInit ' + jobInit);
 
-const previewHref = document.querySelectorAll(".js-preview-href");
+
+const previewHref = document.querySelectorAll('.js-preview-href');
 console.log(previewHref);
-const hrefInit = "#";
+const hrefInit = '#';
 
 // OPTIMIZAR SEGÚN VIDEO MIGUEL 20 oct
 chargeData();
@@ -85,14 +86,15 @@ function getInfo(event) {
 function paint() {
 	previewText[0].innerHTML = data.name || nameInit;
 	previewText[1].innerHTML = data.job || jobInit;
-	previewHref[0].href = "tel:" + data.phone || hrefInit;
-	previewHref[1].href = "mailto:" + data.email || hrefInit;
-	previewHref[2].href = "https://" + data.linkedin || hrefInit;
-	previewHref[3].href = "https://github.com/" + data.github || hrefInit;
+	previewHref[0].href = 'tel:' + data.phone || hrefInit;
+	previewHref[1].href = 'mailto:' + data.email || hrefInit;
+	previewHref[2].href = 'https://' + data.linkedin || hrefInit;
+	previewHref[3].href = 'https://github.com/' + data.github || hrefInit;
+	profileImage.style.backgroundImage =
+		`url(${data.photo})` || 'url(../assets/images/NAN-card-photo-default.jpg)';
 }
 
 // 	if (event.currentTarget.id === 'name') {
-// 		// PREGUNTA #1 (revisar Trello):
 // 		if (event.currentTarget.value !== '') {
 // 			previewText[0].innerHTML = data.name;
 // 		} else {
@@ -131,45 +133,46 @@ function paint() {
 // }
 
 for (const eachElement of inputList) {
-	eachElement.addEventListener("keyup", getInfo);
+	eachElement.addEventListener('keyup', getInfo);
 }
 
 // END Text input
 
 // Reset
-const btnReset = document.querySelector(".js-reset");
+const btnReset = document.querySelector('.js-reset');
 
 function handleReset() {
-	console.log("reset");
+	console.log('reset');
 	data.name = nameInit;
 	data.job = jobInit;
 	data.phone = hrefInit;
 	data.email = hrefInit;
 	data.linkedin = hrefInit;
 	data.github = hrefInit;
+	data.photo = '../assets/images/NAN-card-photo-default.jpg';
 	console.log(data);
 	for (const input of inputList) {
-		input.value = "";
+		input.value = '';
 	}
 	paint();
 	storeData();
 }
 
-btnReset.addEventListener("click", handleReset);
+btnReset.addEventListener('click', handleReset);
 
 // END Reset
 
 // Local Storage
 function storeData() {
-	console.log("guarda");
+	console.log('guarda');
 	const jsonData = JSON.stringify(data);
-	localStorage.setItem("filledData", jsonData);
+	localStorage.setItem('filledData', jsonData);
 	console.log(jsonData);
 }
 
 function chargeData() {
-	console.log("carga");
-	const storedData = localStorage.getItem("filledData");
+	console.log('carga');
+	const storedData = localStorage.getItem('filledData');
 	console.log(storedData);
 	const lastData = JSON.parse(storedData);
 	if (lastData !== null) {
@@ -184,11 +187,11 @@ function chargeData() {
 // card generator
 
 // constante selectora del boton CREAR TARJETA
-const submitButton = document.querySelector(".js-submit");
+const submitButton = document.querySelector('.js-submit');
 // constante selectora del ELEMENTO html con la URL
-const responseURL = document.querySelector(".js-response");
+const responseURL = document.querySelector('.js-response');
 // constante selectora del formulario completo
-const form = document.querySelector(".js-form");
+const form = document.querySelector('.js-form');
 
 // ¿?
 function sendData() {
@@ -197,12 +200,12 @@ function sendData() {
 
 // Función para envío de datos a la API
 function sendRequest() {
-	console.log("entro en funcion antes de fetch");
-	fetch("https://us-central1-awesome-cards-cf6f0.cloudfunctions.net/card/", {
-		method: "POST",
+	console.log('entro en funcion antes de fetch');
+	fetch('https://us-central1-awesome-cards-cf6f0.cloudfunctions.net/card/', {
+		method: 'POST',
 		body: JSON.stringify(data),
 		headers: {
-			"content-type": "application/json",
+			'content-type': 'application/json',
 		},
 	})
 		.then(function (resp) {
@@ -216,21 +219,21 @@ function sendRequest() {
 		});
 }
 
-const hiddenBox = document.querySelector(".js-share-url");
+const hiddenBox = document.querySelector('.js-share-url');
+const inactiveButton = document.querySelector('.js-share');
 
 function showURL(result) {
 	if (result.success) {
 		responseURL.innerHTML = result.cardURL;
 		responseURL.href = result.cardURL;
-		hiddenBox.classList.remove("hidden");
+		hiddenBox.classList.remove('hidden');
+		inactiveButton.classList.add('inactive');
+		submitButton.removeEventListener('click', sendRequest);
 	} else {
-		responseURL.innerHTML = "ERROR:" + result.error;
+		responseURL.innerHTML = 'ERROR:' + result.error;
 	}
-  // const handleForm = function (ev) {
-  //   ev.preventDefault();
-  // }
 }
-submitButton.addEventListener("click", sendRequest);
+submitButton.addEventListener('click', sendRequest);
 
 // END card generator
 
